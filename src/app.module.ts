fc95fa4f39f26dc3c16620f3ac22e3fa96b6d972
kou <contact@awesome-linus.com>
2021-09-08 18:08:37 +0900

    add graphql apollo-server

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { Resolver, Query } from '@nestjs/graphql';
+
+@Resolver()
+export class FooResolver {
+  @Query(() => String)
+  sayHello(): string {
+    return 'Hello World!';
+  }
+}
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      playground: true,
+      autoSchemaFile: 'schema.graphql',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FooResolver],
 })
 export class AppModule {}
