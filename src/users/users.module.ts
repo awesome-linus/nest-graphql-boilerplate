ed30e57b8cb1d06c4d35dbbeec30d5f652e31c76
kou <contact@awesome-linus.com>
2021-09-12 10:08:04 +0900

    add user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { UsersResolver } from './users.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './users.controller';
+import { User } from './users.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule],
+  providers: [UsersResolver],
+  controllers: [UsersController],
+})
 export class UsersModule {}
